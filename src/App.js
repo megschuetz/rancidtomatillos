03210812bcb5dab0e5ebbb8f671d04a43cb101b0
Megan Schuetz <mschuetz411@gmail.com>
2022-07-10 11:31:29 -0600

    resize backdrop covers and add ternary conditional rendering and start video code

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
       .then(response => response.json())
       .then(data => this.setState({ movies: data.movies }))
-      .catch(error => console.log(error))
+      .catch(error => alert(`Error: ${error}`))
   }
 
   handleMovieClick = (id) => {
@@ -34,10 +34,13 @@ class App extends Component {
         <header>
           <h1>Rancid Tomatillos</h1>
         </header>
-        {this.state.singleMoviePreview && <MovieDetails handleClose={this.handleClose} id={this.state.id}/>}
-        <main>
-          <Movies movies={this.state.movies} handleMovieClick={this.handleMovieClick}/>
-        </main>
+        {this.state.singleMoviePreview ? 
+          <MovieDetails handleClose={this.handleClose} id={this.state.id}/> 
+        : 
+          <main>
+            <Movies movies={this.state.movies} handleMovieClick={this.handleMovieClick}/>
+          </main>
+        }
       </body>
     );
   }
--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 import './MovieDetails.css'
-import {movieDeets} from './movieData'
 
 class MovieDetails extends Component {
   constructor() {
     super()
     this.state = {
-       singleMovieDetails: {}
+       singleMovieDetails: {},
+       singleMovieVideo: {}
     }
   }
 
@@ -14,20 +14,20 @@ class MovieDetails extends Component {
     fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.id}`)
       .then(response => response.json())
       .then(data => this.setState({ singleMovieDetails: data.movie }))
-    console.log('componentdidmount  here')
+      .catch(error => alert(`Error: ${error}`))
   }
 
   render() {
 
     const backgroundImage = {
-      backgroundImage: `url(${this.state.singleMovieDetails.backdrop_path})`
+      backgroundImage: `url(${this.state.singleMovieDetails.backdrop_path})`,
+      backgroundSize: 'cover'
     }
 
     return (
       <main className='movie-details' style={backgroundImage}>
         <button className='close-button' onClick={() => this.props.handleClose()}>X</button>
         <div className='movie-box'>
-          <section className='video-box'>Video Box</section>
           <section className='details-box'>
             <h2>Title: {this.state.singleMovieDetails.title}</h2>
             <p className='overview'>Overview: {this.state.singleMovieDetails.overview}</p>
@@ -50,3 +50,24 @@ class MovieDetails extends Component {
 }
 
 export default MovieDetails;
+
+//VIDEO START
+
+// {/* <video width="320" height="240" className='video-box' controls>
+// <source src={`https://www.youtube.com/embed/${this.state.singleMovieVideo.key}`}></source>
+// </video> */}
+
+
+// {/* <iframe
+// width="853"
+// height="480"
+// src={`https://www.youtube.com/embed/${this.state.singleMovieVideo.key}`}
+// frameBorder="0"
+// allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+// allowFullScreen
+// title="Embedded youtube"
+// /> */}
+
+// fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.id}/videos`)
+// .then(response => response.json())
+// .then(data => this.setState({ singleMovieVideo: data.videos }))
