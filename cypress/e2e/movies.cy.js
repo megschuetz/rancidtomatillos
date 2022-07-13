6a0e2fa914f784ae452ba49f6a750bc0ac2f9e10
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-07-13 16:59:07 -0600

    Add cypress testing to movie details

--- cypress/e2e/movies.cy.js
+++ cypress/e2e/movies.cy.js
@@ -4,13 +4,30 @@ describe('Movies Display Page', () => {
   })
     it('Should be able to visit the home page and view a gallery of movies', () => {
         cy.contains('RANCID TOMATILLOS')
-        .get('main')
+          .get('main')
     });
 
     it('Should be able to click on a movie and be taken to the movie details page', () => {
         cy.get('.preview').contains('Ava').click()
-        .url().should('include', '/movies/539885')
+          .url().should('include', '/movies/539885')
     });
+
+    it('Should be able to click on the back button on movie details to return to the home page', () => {
+      cy.get('.preview').contains('Ava').click()
+        .url().should('include', '/movies/539885')
+        .get('.close-button').click()
+        .url().should('include', '/')
+  });
+
+    it('Should be able to play a youtube video from the movie details page', () => {
+      cy.get('.preview').contains('Ava').click()
+        .url().should('include', '/movies/539885')
+        .get('iframe').click({force: true})
+        .url().should('include', '/')
+  });
+
+    
+    
 
     it('Should be able to visit the home page and view a gallery of movies', () => {
       cy.intercept(
