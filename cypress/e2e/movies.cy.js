680afdf46c1a89e22219ff047df7282f956395cc
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-07-13 16:46:54 -0600

    Add home page cypress tests

--- cypress/e2e/movies.cy.js
+++ cypress/e2e/movies.cy.js
@@ -1,19 +1,24 @@
 describe('Movies Display Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
     it('Should be able to visit the home page and view a gallery of movies', () => {
-      cy.visit('http://localhost:3000/')
-        .contains('RANCID TOMATILLOS')
+        cy.contains('RANCID TOMATILLOS')
         .get('main')
-        // .get('grid')
-        // .contains('Please Sign In');
+    });
+
+    it('Should be able to click on a movie and be taken to the movie details page', () => {
+        cy.get('.preview').contains('Ava').click()
+        .url().should('include', '/movies/539885')
     });
 
     it('Should be able to visit the home page and view a gallery of movies', () => {
       cy.intercept(
         {
-          method: 'GET', // Route all GET requests
-          url: '/movies/', // that have a URL that matches '/users/*'
+          method: 'GET', 
+          url: '/movies/', 
         },
-        [] // and force the response to be: []
+        [] 
       ).as('getMovies') 
     });
   });
